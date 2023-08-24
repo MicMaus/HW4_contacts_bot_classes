56802a5aabb46202c1b361bbfabf84621d1fac81
MicMaus <michael.yasynskyi@gmail.com>
2023-08-24 10:01:21 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,46 +7,40 @@ import re
 class Field:
     def __init__(self, value) -> None:
         self.value = value
+    
+    def __str__(self):
+        return self.value
 
 class Name(Field):
     name = 'UserName'
 
 class Phone(Field):
-    phone = 'PhoneNumber'
-
     def __init__(self, value) -> None:
-        super().__init__(str(value))
-
-    def __str__(self):
-        return self.value 
-
+        super().__init__(str(value)) 
 
 class Record:
     def __init__(self, name, phone=None): #mandatory and optional atributes
         self.name = Name(value=name)
         self.phones = []
-        if phone is not None:
+        if phone:
             self.add_new_phone(phone)
-        else:
-           raise CustomError("number already assigned to the contact, add new number")
 
     def add_new_phone(self, phone): #add phone number to the list
         self.phones.append(Phone(value=phone))
 
     def amend_phone(self, name, phone): #empty list and add new phone number
-        for contact_name in phone_book.keys():
-            if name == contact_name:
-                self.phones.clear()
-                self.add_new_phone(phone)
+        self.phones.clear()
+        self.add_new_phone(phone)
 
     def remove_phone(self, phone):
-        phone_str = str(phone)
+        phone_found = False
         for stored_phone in self.phones.copy():
-            if str(stored_phone) == phone_str:
+            if str(stored_phone) == phone:
                 self.phones.remove(stored_phone)
-            else:
-                raise CustomError("phone number was not found")
+                phone_found = True
 
+        if not phone_found:
+            raise CustomError("phone number was not found")
 
 class AddressBook(UserDict):
     def add_record(self, record: Record):
@@ -68,62 +62,63 @@ def parse_input(user_input):
 
 #adding new contact/phone number
 def add_contact (request, name, phone): 
-    if name: #handle situation when name is not provided at all
-        if name not in phone_book: #if contact is new, new Record instance to be created
-            record = Record(name, Phone(value=phone))
-            phone_book.add_record(record)
-            return("new contact successfully added")
-        elif name in phone_book:
-            record = phone_book[name]
-            record.add_new_phone(Phone(value=phone))
-            return("new phone number successfully added to existing contact")
-    else:
+    if not name:  #handle situation when name is not provided at all
         raise CustomError("please provide name and phone number divided by space")
-
+    elif name not in phone_book:
+        record = Record(name, phone)
+        phone_book.add_record(record)
+        return("new contact successfully added")
+    else:
+        record = phone_book[name]
+        if not phone.isdigit():
+            raise CustomError("please provide name and phone number divided by space")
+        record.add_new_phone(phone)
+        return("new phone number successfully added to existing contact")
+          
 #change the phone number
-def change_phone (request, name, phone): 
-    if name and phone:
-        if name in phone_book:
-            record = phone_book[name]
-            record.amend_phone(name, phone)
-            return("contact successfuly changed")
-        else:
-            raise CustomError("contact not found")
-    else:
+def change_phone (request, name, phone):
+    if name not in phone_book:
+        raise CustomError('name not found')
+    if not phone:
         raise CustomError("please provide name and phone number divided by space")
+    
+    record = phone_book[name]
+    record.amend_phone(name, phone)
+    return("contact successfully changed")
+    
 #remove the phone number   
 def delete_phone(request, name, phone):
-    if name and phone:
-        record = phone_book[name]
-        phone_to_remove = Phone(value=phone)
-        record.remove_phone(phone_to_remove)
-        return("phone number successfully removed")
-    else:
+    if name not in phone_book:
+        raise CustomError('name not found')
+    if not phone:
         raise CustomError("please provide name and phone number divided by space")
+    
+    record = phone_book[name]
+    record.remove_phone(phone)
+    return("phone number successfully removed")
 
 #show the phone of user
-def phone (notused1, name, notused2): 
-    if name in phone_book:
-        record = phone_book[name]
-        phone_numbers = []
-        
-        for item in record.phones:
-            phone_numbers.append(item.value)
-
-        for el in phone_numbers:
-            if el.isdigit():
-                return f"{name}: {', '.join(phone_numbers)}"
-        else:
-            return f"{name}: no phone numbers"
-
-    else:
+def show_phone (notused1, name, notused2): 
+    if name not in phone_book:
         raise CustomError("please provide a valid name")
+    
+    record = phone_book[name]
+    phone_numbers = []
+        
+    for item in record.phones:
+        phone_numbers.append(item.value)
+
+    for el in phone_numbers:
+        if el.isdigit():
+            return f"{name}: {', '.join(phone_numbers)}"
+    else:
+        return f"{name}: no phone numbers"
 
 #show all name-phone pairs
 def show_all(notused1, notused2, notused3):
     contacts = []
     for name in phone_book.keys():
-        one_contact = phone(None, name, None)
+        one_contact = show_phone(None, name, None)
         contacts.append(one_contact)
     if contacts:
         return ';\n'.join(contacts)
@@ -150,7 +145,7 @@ commands = {
     "add": add_contact,
     "change": change_phone,
     "remove": delete_phone,
-    "phone": phone,
+    "phone": show_phone,
     "show all": show_all,
     "hello": hello,
     "search": search,
